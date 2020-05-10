2364de90f5c511e6de67b6c3ba9a518c2ffc4a6e
Aditya Raj <aditya.del222@gmail.com>
2020-05-10 23:04:12 +0530

    Updated About

--- scripts/disp_about.js
+++ scripts/disp_about.js
@@ -1,4 +1,4 @@
-const aboutMe = `Lorem ipsum dolor sit amet consectetur... <br /><br />What? No, wait.<br /><br />Sometimes I forget to switch my mindset from development to production.<br /><br />Hey, there! Glad you found me here.<br />Not much of an Einstein but I love to create stuff, and here's my profile for a start.<br /><br />Hope you don't hate it.`;
+const aboutMe = `Lorem ipsum dolor sit amet consectetur... <br /><br />What? No, wait.<br /><br />Pardon me but sometimes I forget to switch my mindset from development to production.<br /><br />Hey, there! Glad you found me here.<br />Not much of an Einstein but I love to create stuff, and here's my profile for a start.<br /><br />Hope you don't hate it.`;
 document.getElementById('about-info-p').innerHTML = aboutMe;
 const display_more_about = () => {
     document.getElementsByClassName('view-more')[0].style.display = 'none';
