a584a99e662062a2d1af098da5c1e6c1b3697fa1
Aditya Raj <aditya.del222@gmail.com>
2020-05-10 22:36:32 +0530

    Final commit

--- scripts/fetch_data.js
+++ scripts/fetch_data.js
@@ -1,18 +1,18 @@
 window.onload = () => {
     fetch('./data/experiences.json')
-        .then(res_exp => res_exp.json())
-        .then(data_exp => display_experiences(data_exp))
-        .catch(err_exp => console.error(err_exp));
+        .then(res => res.json())
+        .then(data => display_experiences(data))
+        .catch(err => console.error(err));
     fetch('./data/education.json')
-        .then(res_edu => res_edu.json())
-        .then(data_edu => display_educations(data_edu))
-        .catch(err_edu => console.error(err_edu));
+        .then(res => res.json())
+        .then(data => display_educations(data))
+        .catch(err => console.error(err));
     fetch('./data/projects.json')
-        .then(res_proj => res_proj.json())
-        .then(data_proj => display_projects(data_proj))
-        .catch(err_proj => console.error(err_proj));
+        .then(res => res.json())
+        .then(data => display_projects(data))
+        .catch(err => console.error(err));
     fetch('./data/books.json')
-        .then(res_book => res_book.json())
-        .then(data_book => display_books(data_book))
-        .catch(err_book => console.error(err_book));
+        .then(res => res.json())
+        .then(data => display_books(data))
+        .catch(err => console.error(err));
 }
